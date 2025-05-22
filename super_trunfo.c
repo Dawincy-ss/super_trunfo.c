e95292e087b007cb7ee1fdf0bd32ad4e59585112
Dawincy-ss <dawincycraft2013@gmail.com>
2025-05-22 00:11:55 +0000

    Criação de variaveis

---
+++ super_trunfo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+    char estado1, estado2;    // Variaveis referents as informaçãos presentes nas cartas
+    char codigo1[5], codigo2[5];         // As quais seriam: Estado, Codigo da carta, Nome da Cidade, População, Area em km², PIB e Número de pontos turisticos. 
+    char cidade1[50], cidade2[50];
+    int população1, população2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos1, pontos2;
+
+    printf("Bem-Vindo ao Super Trunfo city's! \n");
+    printf("Cadastre a suas cartas abaixo: \n");
+
+
+
+
+
+
+}
